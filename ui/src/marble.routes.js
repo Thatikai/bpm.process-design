1889eb0cdfaa95560eee6f94ed114ff311ae614d
saiteja yadanaparthi <saiteja.y@above-inc.com>
2016-12-27 13:44:50 +0530

    resolved merge conflicts

--- ui/src/marble.routes.js
+++ ui/src/marble.routes.js
@@ -10,17 +10,10 @@ angular
         '$stateProvider',
         function ($stateProvider) {
             'use strict';
-<<<<<<< HEAD
             $stateProvider.state('base',
                     {
                         url: '/',
                         templateUrl: '/marble/marble.html'
-=======
-            $stateProvider.state('home',
-                    {
-                        url: '/home',
-                        templateUrl: '/marble/marble-home.html'
->>>>>>> f181a2767d8699d29731442bbc8d030632128203
                     }
                 );
         }
